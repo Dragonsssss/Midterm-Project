831c4ca9cd94bd63818f5beaee281dd5d1307597
Dragonsssss <31480300+Dragonsssss@users.noreply.github.com>
2018-11-27 23:26:47 +0800

    666

--- code.cpp
+++ code.cpp
@@ -11,7 +11,7 @@ struct Point
 const int MAX_cp = 2000; // max quantity of the changing points
 
 double Distance(Point a, Point b);
-bool generate_changingpoint(int i, int Cnt1, Point start, Point end, Point changingpoint[], int d);
+bool generate_changingpoint(int i, int j, int Cnt1, Point start, Point end, Point changingpoint[], int d);
 int generate_route(Point start, Point end, Point changingpoint[], Point route[], int Cnt1);
 double calculate_routeCost(Point route[], int X[], int Y[], int R[], int P[], int Cnt1, int Cnt2, int w, int m);
 int main()
@@ -39,34 +39,46 @@ int main()
 	Point best_changingpoint[MAX_cp] = {0};
 	double lowest_routeCost = 50000000;
 	int Cnt1 = 0; // Cnt1 is the quantity of changing points
-	while(changingpoint[Cnt1-1].x != end.x and changingpoint[Cnt1-1].y != end.y)
-	{
+	double Dst = Distance(start, end);
+	double radius = Dst / 100 + 1;
+	while(true)
+	{	
 		Cnt1 += 1;
-		bool flag = 0;		
-		for(int i = 0; i < 8; i++)
+		bool flag = 0;	
+		for(int i = 0; i < radius; i++)
 		{
-			// changing point
-			bool available = generate_changingpoint(i, Cnt1, start, end, changingpoint, d);
-			if(available == 0) // it is not available
+			for(int j = 0; j < radius; j++)
 			{
-				continue;
-			}			
-
-			// route
-			Point route[2*MAX_n] = {0};
-			int Cnt2 = generate_route(start, end, changingpoint, route, Cnt1); 
-			// Cnt2 is the quantity of points on the route
-			
-			// route cost
-			double routeCost = calculate_routeCost(route, X, Y, R, P, Cnt1, Cnt2, w, m);
-			if(routeCost < lowest_routeCost)
-			{
-				flag = 1;
-				lowest_routeCost = routeCost;
-				best_changingpoint[Cnt1-1].x = changingpoint[Cnt1-1].x;
-				best_changingpoint[Cnt1-1].y = changingpoint[Cnt1-1].y;
+				
+				if((i*i + j*j) >= Dst or (i == 0 and j == 0))
+				{
+					continue;
+				}
+				
+				// changing point
+				bool available = generate_changingpoint(i, j, Cnt1, start, end, changingpoint, d);
+				if(available == 0) // it is not available
+				{
+					continue;
+				}
+				
+				// route
+				Point route[2*MAX_n] = {0};
+				int Cnt2 = generate_route(start, end, changingpoint, route, Cnt1); 
+				// Cnt2 is the quantity of points on the route				
+				
+				// route cost
+				double routeCost = calculate_routeCost(route, X, Y, R, P, Cnt1, Cnt2, w, m);				
+				if(routeCost < lowest_routeCost)
+				{
+					flag = 1;
+					lowest_routeCost = routeCost;
+					best_changingpoint[Cnt1-1].x = changingpoint[Cnt1-1].x;
+					best_changingpoint[Cnt1-1].y = changingpoint[Cnt1-1].y;												
+				}
 			}
 		}
+		
 		if(flag == 1)
 		{
 			changingpoint[Cnt1-1].x = best_changingpoint[Cnt1-1].x;
@@ -76,9 +88,15 @@ int main()
 		{
 			Cnt1 -= 1;
 			break;
-		}
+		}		
+		
+		if(changingpoint[Cnt1-1].x == end.x or changingpoint[Cnt1-1].y == end.y)
+		{
+			break;
+		}  
 	}
 	
+
 	// print answer
 	cout << Cnt1 << " ";
 	for(int i = 0; i < Cnt1 - 1; i++)
@@ -89,11 +107,13 @@ int main()
 	
 	return 0;
 }
+
 double Distance(Point a, Point b)
 {
 	return sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2)); 
 }
-bool generate_changingpoint(int i, int Cnt1, Point start, Point end, Point changingpoint[], int d)
+
+bool generate_changingpoint(int i, int j, int Cnt1, Point start, Point end, Point changingpoint[], int d)
 {
 	int x_sign = (end.x - start.x) / abs(end.x - start.x);
 	int y_sign = (end.y - start.y) / abs(end.y - start.y);
@@ -106,78 +126,40 @@ bool generate_changingpoint(int i, int Cnt1, Point start, Point end, Point chang
 	{
 		changingpoint[Cnt1-1].x = changingpoint[Cnt1-2].x;
 		changingpoint[Cnt1-1].y = changingpoint[Cnt1-2].y;
-	}
+	}	
 	
-	if(i == 0)
-	{
-		changingpoint[Cnt1-1].x += 1 * x_sign;
-		changingpoint[Cnt1-1].y += 0 * y_sign;
-	}
-	else if(i == 1)
-	{
-		changingpoint[Cnt1-1].x += 2 * x_sign;
-		changingpoint[Cnt1-1].y += 0 * y_sign;		
-	}
-	else if(i == 2)
-	{
-		changingpoint[Cnt1-1].x += 0 * x_sign;
-		changingpoint[Cnt1-1].y += 1 * y_sign;			
-	}
-	else if(i == 3)
-	{
-		changingpoint[Cnt1-1].x += 1 * x_sign;
-		changingpoint[Cnt1-1].y += 1 * y_sign;			
-	}
-	else if(i == 4)
-	{
-		changingpoint[Cnt1-1].x += 2 * x_sign;
-		changingpoint[Cnt1-1].y += 1 * y_sign;			
-	}
-	else if(i == 5)
-	{
-		changingpoint[Cnt1-1].x += 0 * x_sign;
-		changingpoint[Cnt1-1].y += 2 * y_sign;			
-	}
-	else if(i == 6)
-	{
-		changingpoint[Cnt1-1].x += 1 * x_sign;
-		changingpoint[Cnt1-1].y += 2 * y_sign;		
-	}
-	else if(i == 7)
-	{
-		changingpoint[Cnt1-1].x += 2 * x_sign;
-		changingpoint[Cnt1-1].y += 2 * y_sign;		
-	}
+	changingpoint[Cnt1-1].x += i * x_sign;
+	changingpoint[Cnt1-1].y += j * y_sign;
 	
 	if(x_sign > 0 and y_sign > 0)
 	{
-		if(changingpoint[Cnt1-1].x >= end.x and changingpoint[Cnt1-1].y >= end.y)
+		if(changingpoint[Cnt1-1].x > end.x or changingpoint[Cnt1-1].y > end.y)
 		{
 			return 0;
 		}			
 	}
 	else if(x_sign > 0 and y_sign < 0)
 	{
-		if(changingpoint[Cnt1-1].x >= end.x and changingpoint[Cnt1-1].y <= end.y)
+		if(changingpoint[Cnt1-1].x > end.x or changingpoint[Cnt1-1].y < end.y)
 		{
 			return 0;
 		}			
 	}
 	else if(x_sign < 0 and y_sign > 0)
 	{
-		if(changingpoint[Cnt1-1].x <= end.x and changingpoint[Cnt1-1].y >= end.y)
+		if(changingpoint[Cnt1-1].x < end.x or changingpoint[Cnt1-1].y > end.y)
 		{
 			return 0;
 		}			
 	}
 	else if(x_sign < 0 and y_sign < 0)
 	{
-		if(changingpoint[Cnt1-1].x <= end.x and changingpoint[Cnt1-1].y <= end.y)
+		if(changingpoint[Cnt1-1].x < end.x or changingpoint[Cnt1-1].y < end.y)
 		{
 			return 0;
 		}			
 	}
-	
+		
 	double sum_d = 0;
 	sum_d += Distance(start, changingpoint[0]);
 	for(int i = 1; i < Cnt1 - 1; i++)
@@ -193,8 +175,9 @@ bool generate_changingpoint(int i, int Cnt1, Point start, Point end, Point chang
 	else
 	{
 		return 1;
-	}
+	}	
 }
+
 int generate_route(Point start, Point end, Point changingpoint[], Point route[], int Cnt1)
 {
 	Point totalpoint[MAX_cp + 2] = {0};
@@ -240,8 +223,9 @@ int generate_route(Point start, Point end, Point changingpoint[], Point route[],
 			tmp = Dst + tmp - static_cast<int>(Dst+tmp);
 		}
 	}
-	return Cnt2; // Cnt2 is the changing point quantity that need to be calculate risk
+	return Cnt2; 
 }
+
 double calculate_routeCost(Point route[], int X[], int Y[], int R[], int P[], int Cnt1, int Cnt2, int w, int m)
 {
 	double routeRisk = 0, dist = 0;
@@ -254,7 +238,10 @@ double calculate_routeCost(Point route[], int X[], int Y[], int R[], int P[], in
 				routeRisk += P[i] * (R[i] - dist)/ R[i];
 		}
 	}
+	
 	double routeCost = 0;
-	routeCost = routeRisk + w * Cnt1; 
+	routeCost = routeRisk + w * Cnt1;
+	
 	return routeCost;
-}
+}		
+		
